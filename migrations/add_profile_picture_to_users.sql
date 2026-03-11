3324ee5053f0399f62e36935196ba139aa9f439c
de la Cruz Diaz, Rubén <rcruzd@minsait.com>
2026-03-11 13:47:57 +0100

    chore: remove files specified in .gitignore from repository

--- migrations/add_profile_picture_to_users.sql
+++
@@ -1,8 +0,0 @@
--- Migración: Añadir campo profile_picture a la tabla users
--- Fecha: 2026-01-17
--- Descripción: Añade columna para almacenar base64 de foto de perfil o URL
-
-ALTER TABLE users
-ADD COLUMN profile_picture TEXT;
-
-COMMENT ON COLUMN users.profile_picture IS 'Base64 de la foto de perfil o URL';
